af4a3b72a6d6da6a1b2a651f24b344582c5b6c1a
Matt Gibson <gothick@gothick.org.uk>
2020-01-29 18:02:59 +0000

    More room tests added

--- tests/testroom.py
+++ tests/testroom.py
@@ -3,6 +3,8 @@ from unittest.mock import Mock
 # TODO Mock items and factory?
 from item import Item, ItemFactory
 from room import Room
+from words import DIRECTIONS
+from words import Noun
 
 item_data = {
     "bike": {
@@ -24,7 +26,7 @@ room_data = {
             "basic": "Basic description"
         },
         "exits": {
-            "south": {
+            Noun.SOUTH: {
                 "destination": "livingroom"
             }
         }
@@ -46,15 +48,19 @@ room_data = {
             ]
         },
         "exits": {
-            "north": {
+            Noun.NORTH: {
                 "destination": "simpleroom",
+                "transition": "Northerly transition",
                 "rules": [
                     {
-                        # We don't want to let the player carry the penny-farthing into
-                        # the house. 
-                        "type": "not_if_carrying",
+                        "type": "not_if_riding",
                         "item": "bike",
-                        "objection": "The penny-farthing won't fit through there."
+                        "objection": "You can't ride that thorugh there."
+                    },
+                    {
+                        "type": "not_if_carrying",
+                        "item": "simple_item",
+                        "objection": "You can't carry that through there."
                     }
                 ]
             }
@@ -67,15 +73,67 @@ class TestRoomMethods(unittest.TestCase):
         self.item_factory = ItemFactory(item_data)
         self.rooms = {}
 
-        # Build the universe.
+        # Build our little universe.
         for id, data in room_data.items():
             self.rooms[id] = Room(id, self.item_factory, data)
 
-    def test_description(self):
+    def test_basic_exits(self):
         room = self.rooms["simpleroom"]
         player = Mock()
-        (result, _) = room.can_go("north", player)
+        (result, _) = room.can_go(Noun.NORTH, player)
         self.assertFalse(result, "Shouldn't be able to go north when that exit doesn't exist")
+
+        (result, _) = room.can_go(Noun.SOUTH, player)
+        self.assertTrue(result, "Should be able to exit south")
+
+        self.assertEqual(room.room_id_from_exit(Noun.SOUTH), "livingroom")
+        self.assertEqual(room.room_id_from_exit(Noun.WEST), None)
+
+        room = self.rooms["livingroom"]
+        self.assertEqual(room.transition_from_exit(Noun.NORTH), "Northerly transition")
+        self.assertEqual(room.transition_from_exit(Noun.SOUTH), None)
+
+
+    def test_can_go_conditions(self):
+        room = self.rooms["livingroom"]
+        attrs = {
+            'is_riding.return_value': False,
+            'has.return_value': False
+        }
+        player = Mock(**attrs)
+        (result, _) = room.can_go(Noun.NORTH, player)
+        self.assertTrue(result, "Basic player should be able to go north unimpeded")
+
+        attrs = {
+            'is_riding.return_value': True,
+            'has.return_value': False
+        }
+        player = Mock(**attrs)
+        (result, message) = room.can_go(Noun.NORTH, player)
+        self.assertFalse(result, "Player riding a bike should be impeded by rule")
+
+        attrs = {
+            'is_riding.return_value': False,
+            'has.return_value': True
+        }
+        player = Mock(**attrs)
+        (result, message) = room.can_go(Noun.NORTH, player)
+        self.assertFalse(result, "Player carrying something be impeded by rule")
+    
+    def test_description(self):
+        room = self.rooms["simpleroom"]
+        desc = room.description
+        self.assertIn("Basic description", desc, "Room's description attribute doesn't contain its basic description")
+        self.assertIn("south", desc, "Room's description attribute doesn't contain a known exit")
+
+        room = self.rooms["livingroom"]
+        desc = room.description
+        self.assertIn("There is a bike here", desc, "Inventory item not listed in room description")
+        self.assertIn("By the front door rests a bike", desc, "Room extra wasn't present in description")
+        room.take("bike")
+        desc = room.description
+        self.assertNotIn("By the front door rests a bike", desc, "Conditional room extra doesn't disappear when item removed")
+
 
     def test_can_go(self):
         pass
